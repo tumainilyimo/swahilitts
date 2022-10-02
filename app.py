baf608cd9cf5590325a98c6e4ac4fa840600ead9
tumainilyimo <lyimotumaini@gmail.com>
2022-10-02 17:32:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,14 +23,28 @@ def transcribe(audio):
     return result.text
     
     
- 
-gr.Interface(
-    title = 'OpenAI Whisper ASR Gradio Web UI', 
-    fn=transcribe, 
-    inputs=[
-        gr.inputs.Audio(source="microphone", type="filepath")
-    ],
-    outputs=[
-        "textbox"
-    ],
-    live=True).launch()
+import whisper
+import streamlit as st
+
+model = whisper.load_model("small")
+
+st.title("OpenAI Whisper ASR Streamlit Web UI")
+
+audio = st.file_uploader("Upload an audio file", type="wav")
+
+if audio is not None:
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    st.write(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16 = False)
+    result = whisper.decode(model, mel, options)
+    st.write(result.text)
